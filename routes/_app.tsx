006d6531924f1eb11c6c0fdd5e7e45ab601a54c6
Yoshiya Hinosawa <stibium121@gmail.com>
2025-04-03 11:11:06 +0900

    chore: add favicon

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,6 +8,7 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>node_test_viewer</title>
         <link rel="stylesheet" href="/styles.css" />
+        <link rel="icon" href="/node.svg" />
       </head>
       <body>
         <Component />
