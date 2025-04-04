fdc2876b60af4d9d2e23358348f49ed2c3ddb75f
Yoshiya Hinosawa <stibium121@gmail.com>
2025-04-04 15:08:16 +0900

    feat: show error reasons

--- islands/ReportTable.tsx
+++ islands/ReportTable.tsx
@@ -7,6 +7,7 @@ export function ReportTable(props: { class?: string; report: DayReport }) {
   const { report } = props;
 
   const testNames = getTestNames(report);
+  const nodeVersion = getNodeVersion(report);
 
   return (
     <table
@@ -47,7 +48,13 @@ export function ReportTable(props: { class?: string; report: DayReport }) {
         return (
           <tr key={testName} class="border-t border-gray-300 font-mono">
             <td colSpan={2} class="text-xs py-1 whitespace-nowrap">
-              {testName}
+              <a
+                href={`https://github.com/nodejs/node/blob/v${nodeVersion}/test/${testName}`}
+                class="hover:text-blue-500"
+                target="_blank"
+              >
+                {testName}
+              </a>
             </td>
             {[linux, windows, darwin].map((result) => (
               <td class="text-center">
@@ -66,6 +73,11 @@ function getTestNames(report: DayReport) {
   return Object.keys(testReport?.results ?? {});
 }
 
+function getNodeVersion(report: DayReport) {
+  const testReport = report.windows ?? report.linux ?? report.darwin;
+  return testReport?.nodeVersion;
+}
+
 function Result(
   props: { result: SingleResult | undefined },
 ) {
@@ -79,14 +91,35 @@ function Result(
   const error = result[1];
   if (error) {
     if ("code" in error) {
-      return <span class="text-red-500">FAIL</span>;
+      return (
+        <span class="text-red-500 relative group">
+          FAIL<Tooltip text={error.stderr} />
+        </span>
+      );
     } else if ("timeout" in error) {
-      return <span class="text-red-500">T/O</span>;
+      return (
+        <span class="text-red-500 relative group">
+          T/O<Tooltip text={`Timed out after ${error.timeout}ms`} />
+        </span>
+      );
     } else if ("message" in error) {
-      return <span class="text-red-500">FAIL</span>;
+      return (
+        <span class="text-red-500 relative group">
+          FAIL<Tooltip text={error.message} />
+        </span>
+      );
     }
   }
-  return "Unknown error";
+
+  return <span class="text-gray-400">INVL</span>;
+}
+
+function Tooltip(props: { text: string }) {
+  return (
+    <pre class="absolute w-[50vw] top-[18px] left-1/2 translate-x-[-65%] overflow-scroll px-4 py-2 text-xs font-mono text-left hidden group-hover:block text-white bg-gray-800 border border-gray-900 rounded shadow-lg z-10">
+      {props.text}
+    </pre>
+  );
 }
 
 function Summary(props: { report: TestReport | undefined }) {
